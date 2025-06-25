a4ead0c0ed2dc47b5f27c4f3da39d74a3149e565
zaid <skzaid8821@gmail>
2025-06-26 01:36:02 +0530

    updated

--- index.js
+++ index.js
@@ -1,34 +1,89 @@
+// import express from "express";
+// const app = express();
+
+// app.use(express.json());
+// const api = [];
+
+// app.get("/user", (req, res) => {
+//   const { name, age } = req.body;
+//   if (!name || !age) {
+//     return res.status(400).send({ message: "enter name as well age" });
+//   }
+//   if (typeof name !== "string" || typeof age !== "number") {
+//     return res.send({
+//       message: "invalid input The name must string and age must be number",
+//     });
+//   }
+
+//   const isSame = api.find((user) => user.name === name);
+//   if (isSame) {
+//     return res.send({
+//       message: "the username already exists try another name",
+//     });
+//   }
+//   api.push({ name, age });
+//   res
+//     .status(200)
+//     .send({ user: `new user with name ${name} age ${age}`, users: api });
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("heeloo");
+// });
+
+// app.listen(3000, () => {
+//   console.log("the server is running on port 3000");
+// });
+
 import express from "express";
 const app = express();
 
 app.use(express.json());
+
 const api = [];
 
-app.get("/user", (req, res) => {
-  const { name, age } = req.body;
-  if (!name || !age) {
-    return res.status(400).send({ message: "enter name as well age" });
-  }
-  if (typeof name !== "string" || typeof age !== "number") {
-    return res.send({
-      message: "invalid input The name must string and age must be number",
-    });
-  }
+app.get("/user", (req, res, next) => {
+  try {
+    const { name, age } = req.body;
+    if (!name || !age) {
+      let err = new Error("enter name as well age");
+      err.status = 400;
+      throw err;
+    }
 
-  const isSame = api.find((user) => user.name === name);
-  if (isSame) {
-    return res.send({
-      message: "the username already exists try another name",
+    if (typeof name !== "string" || typeof age !== "number") {
+      return res.send({
+        message: "invalid input The name must string and age must be number",
+      });
+    }
+
+    const isSame = api.find((user) => user.name === name);
+    if (isSame) {
+      return res.send({
+        message: "the username already exists try another name",
+      });
+    }
+
+    api.push({ name, age });
+    res.status(200).send({
+      user: `new user with name ${name} age ${age}`,
+      users: api,
     });
+  } catch (err) {
+    next(err);
   }
-  api.push({ name, age });
-  res
-    .status(200)
-    .send({ user: `new user with name ${name} age ${age}`, users: api });
 });
 
 app.get("/", (req, res) => {
-  res.send("heeloo");
+  res.send("helllllow");
+});
+
+// ------- global error handler --------
+app.use((err, req, res, next) => {
+  console.log("Caught error:", err.message);
+  res
+    .status(err.status || 500)
+    .send({ error: err.message || "something went wrong" });
 });
 
 app.listen(3000, () => {
